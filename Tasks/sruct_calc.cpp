62bf8c874586e1ff0561e51a6e9afe5d1b75be8a
IamGatheru <davidgatheru490@gmail.com>
2025-05-03 11:30:05 +0300

    Submitted the task

--- Tasks/sruct_calc.cpp
+++ Tasks/sruct_calc.cpp
@@ -7,6 +7,49 @@
 */
 
 #include <cstdio>
-int main() {
+
+enum class Operation {
+  Add,
+  Subtract,
+  Multiply,
+  Divide
+};
+
+struct Calculator{
+
+  Operation op;
   
+  Calculator(Operation operation): op (operation){ }
+  
+  int calculate(int a, int b) {
+
+    switch (op) {
+    case Operation::Add: {
+      return a + b;
+    }break;
+    case Operation::Subtract: {
+      return a - b;
+    }break;
+
+    case Operation::Multiply: {
+      return a * b;
+    }break;
+
+    case Operation::Divide: {
+      if (b < 1) {
+	printf("Not going to happen dawg");
+      }
+      return a / b;
+    }break;
+      
+    }
+    return 0;
+  };
+};
+int main() {
+  Calculator calc(Operation::Add);
+  int sum;
+  sum = calc.calculate(123, 325);
+  printf("The sum is %d", sum);
+  return 0;
 }
